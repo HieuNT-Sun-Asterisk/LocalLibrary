fa68b96c40a16743b51ba21b6e1c858ed9b74dff
HieuNT1998 <trihieua2@gmail.com>
2020-08-06 11:11:56 +0700

    Django Tutorial Part 7

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
+from django.shortcuts import get_object_or_404
+
 
 
 def index(request):
@@ -12,28 +15,41 @@ def index(request):
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+
     # The 'all()' is implied by default.    
     num_authors = Author.objects.count()
-    
     context = {
     'num_books': num_books,
     'num_instances': num_instances,
     'num_instances_available': num_instances_available,
     'num_authors': num_authors,
+    'num_visits': num_visits,
     }
-
     return render(request, 'index.html', context=context)
 
-
-    # context = {
-    #     'num_books': num_books,
-    #     'num_instances': num_instances,
-    #     'num_instances_available': num_instances_available,
-    #     'num_authors': num_authors,
-    # }
-
-    # # Render the HTML template index.html with the data in the context variable
-    # return render(request, 'catalog/index.html', context=context)
-
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 1
     
+    def get_queryset(self):
+        return Book.objects.all()[:5] # Get 5 books containing the title war
 
+class BookDetailView(generic.DetailView):
+    model = Book
+    # pass
+
+
+class AuthorListView(generic.ListView):
+    def get_queryset(self):
+        return Author.objects.all()[:5]
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+def book_detail_view(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'catalog/book_detail.html', context={'book': book})
